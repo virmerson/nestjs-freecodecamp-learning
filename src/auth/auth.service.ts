f725c8b1e552ebc3b6eacbfab93a95ac673efa0f
virmerson <virmerson@gmail.com>
2022-12-29 11:24:55 -0400

    first commit - Start an API with nestjs and prisma

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,68 @@
+import {
+  ForbiddenException,
+  Injectable,
+  UseFilters,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+
+@Injectable()
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+  async signin(dto: AuthDto) {
+    const user =
+      await this.prisma.user.findUnique({
+        where: {
+          email: dto.email,
+        },
+      });
+    if (!user)
+      throw new ForbiddenException(
+        'Credentials incorrect',
+      );
+
+    const pwMatches = await argon.verify(
+      user.hash,
+      dto.password,
+    );
+
+    if (!pwMatches)
+      throw new ForbiddenException(
+        'Credentials incorrect',
+      );
+    delete user.hash;
+
+    return user;
+  }
+
+  async signup(dto: AuthDto) {
+    //generate the password hash
+    const hash = await argon.hash(dto.password);
+    try {
+      //save the new user in the db
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      delete user.hash;
+      //return the saved user
+      return user;
+    } catch (error) {
+      if (
+        error instanceof
+        PrismaClientKnownRequestError
+      ) {
+        if (error.code == 'P2002') {
+          throw new ForbiddenException(
+            'Credentials Taken',
+          );
+        }
+      }
+      throw error;
+    }
+  }
+}
